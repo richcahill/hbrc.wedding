1f40f57a809bcfac30ded52f46fcca39c85648af
Rich Cahill <rich@rc3.me>
2024-09-17 21:39:34 +0100

    add title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
 	import Logo from '$lib/Logo.svelte';
 </script>
 
+<svelte:head>
+	<title>HBRC — Wedding</title>
+</svelte:head>
 <div class="w-screen h-screen bg-zinc-950 flex items-center justify-center">
 	<Logo />
 </div>

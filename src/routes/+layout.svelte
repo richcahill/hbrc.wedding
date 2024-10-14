9c911f5f0cbda120ab6831b01e6e19b01e051923
Rich Cahill <rich@rc3.me>
2024-10-14 19:01:35 +0100

    add anayltics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
 	import '../app.css';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		inject({ mode: dev ? 'development' : 'production' });
+	});
 </script>
 
 <slot />

b8f049f9c001906159da0fa30f2461241c11ae5a
Rich Cahill <rich@rc3.me>
2024-10-30 15:53:18 +0000

    back to the vercel image adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +9,14 @@ const config = {
 			$stores: './src/stores',
 			$routes: './src/routes'
 		},
-		adapter: adapter()
+		adapter: adapter({
+			images: {
+				sizes: [640, 828, 1200, 1920, 3840],
+				formats: ['image/avif', 'image/webp'],
+				minimumCacheTTL: 300,
+				domains: ['hbrc.wedding']
+			}
+		})
 	},
 	preprocess: vitePreprocess()
 };

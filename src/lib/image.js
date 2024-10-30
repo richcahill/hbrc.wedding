eb394c939cd372c6d21ae6a5e9ff1bfddd1711cc
Rich Cahill <rich@rc3.me>
2024-10-30 15:01:27 +0000

    use svelte's built in image handling

--- src/lib/image.js
+++ src/lib/image.js
@@ -1,4 +1,22 @@
 // utils/image.js
-export function optimize(src) {
-	return src; // Return the original image URL without optimization
+import { dev } from '$app/environment';
+
+export function optimize(src, widths = [640, 960, 1280], quality = 90) {
+	// if (dev) return src; // Use original source in development
+
+	// Ensure src is an absolute URL
+	let absoluteURL = src;
+	if (!src.startsWith('http')) {
+		absoluteURL = `https://hbrc.wedding${src}`;
+	}
+
+	return widths
+		.slice()
+		.sort((a, b) => a - b)
+		.map((width, i) => {
+			const url = `/_vercel/image?url=${encodeURIComponent(absoluteURL)}&w=${width}&q=${quality}`;
+			const descriptor = i < widths.length - 1 ? ` ${width}w` : '';
+			return url + descriptor;
+		})
+		.join(', ');
 }

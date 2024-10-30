2a16e87b9d6fec7fba71656ea87e97053fd25bf5
Rich Cahill <rich@rc3.me>
2024-10-30 15:42:58 +0000

    split out the service account secrets from the json

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 // svelte.config.js
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
+// adaper auto
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,22 +11,7 @@ const config = {
 			$stores: './src/stores',
 			$routes: './src/routes'
 		},
-		adapter: adapter({
-			images: {
-				sizes: [640, 828, 1200, 1920, 3840],
-				formats: ['image/avif', 'image/webp'],
-				minimumCacheTTL: 300,
-				domains: ['hbrc.wedding'],
-				remotePatterns: [
-					{
-						protocol: 'https',
-						hostname: 'hbrc.wedding',
-						port: '',
-						pathname: '/images/**'
-					}
-				]
-			}
-		})
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };

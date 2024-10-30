54a9fdb726d18d0febb28aa17dd3fff9f12bc0a5
Rich Cahill <rich@rc3.me>
2024-10-30 11:28:40 +0000

    use vercel image adapter ... for the edgggggeeeee

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
 		alias: {
 			$lib: './src/lib',
 			$stores: './src/stores',
 			$routes: './src/routes'
-		}
+		},
+		adapter: adapter({
+			images: {
+				sizes: [640, 828, 1200, 1920, 3840],
+				formats: ['image/avif', 'image/webp', 'image/jpeg', 'image/png', 'image/gif', 'image/jpg'],
+				minimumCacheTTL: 300,
+				domains: ['example-app.vercel.app']
+			}
+		})
 	},
 	preprocess: vitePreprocess()
 };

69cb932d747825bcce6ed049f72e8af2f861755a
Rich Cahill <rich@rc3.me>
2024-10-30 11:40:46 +0000

    OK this time please vercel image optimiser

--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,7 @@ const config = {
 			images: {
 				sizes: [640, 828, 1200, 1920, 3840],
 				formats: ['image/avif', 'image/webp'],
-				minimumCacheTTL: 300,
-				domains: ['hbrc.wedding']
+				minimumCacheTTL: 300
 			}
 		})
 	},
